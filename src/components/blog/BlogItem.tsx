f18010611e5da591989b5bffd455780283da0f2f
Darren White <dwhite@gentingcasinos.co.uk>
2020-11-25 12:17:01 +0000

    :construction: fix: bolg item showing meta details

--- src/components/blog/BlogItem.tsx
+++ src/components/blog/BlogItem.tsx
@@ -6,28 +6,31 @@ import { useRelativeTime } from '~/hooks';
 type Post = {
   link: string;
   module: {
-    meta: unknown;
+    frontMatter: {
+      title: string;
+      snippet: string;
+      timestamp: number;
+    };
   };
 };
 
 const BlogItem = ({ post }: { post: Post }) => {
   const {
     link,
-    module: { meta },
+    module: { frontMatter },
   } = post;
 
-  console.log('BlogHeroItem -> link', link);
-  //const relativeTime = useRelativeTime(meta.date);
+  const relativeTime = useRelativeTime(frontMatter.timestamp);
 
   return (
     <div>
-      {/* <p className="font-semibold uppercase tracking-wider text-sm leading-5 text-gray-500">
+      <p className="font-semibold uppercase tracking-wider text-sm leading-5 text-gray-500">
         <time dateTime="2020-03-16">{relativeTime}</time>
-      </p> */}
+      </p>
       <Link href={`blog${link}`}>
         <a href="#" className="block">
-          <h3 className="mt-2 text-xl leading-9 font-extrabold tracking-tight ">Boost your conversion rate</h3>
-          {/* <p className="mt-1 text-base leading-7 text-gray-300">{meta.description}</p> */}
+          <h3 className="mt-2 text-xl leading-9 font-extrabold tracking-tight ">{frontMatter.title}</h3>
+          <p className="mt-1 text-base leading-7 text-gray-300">{frontMatter.snippet}</p>
         </a>
       </Link>
       <div className="mt-2">
