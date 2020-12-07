37e56fef8ea45b56611259a92a6fb60d01c210f0
Darren White <dwhite@gentingcasinos.co.uk>
2020-12-07 15:46:51 +0000

    :green_heart: test 6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,10 @@
 import { FC } from 'react';
 
-import { AppShell, Container, IntroHero, BlogHero, Footer } from '~/components';
+import { AppShell, Container } from '~/components';
 
 const IndexPage: FC = () => (
   <AppShell>
-    <Container>
-      <IntroHero />
-    </Container>
-    <Container bg>
-      <BlogHero />
-    </Container>
-    <Container>
-      <Footer />
-    </Container>
+    <Container>Test</Container>
   </AppShell>
 );
 
