88b6be063343d657ba2fe859fb2b1c98f7fb02fd
Darren White <dwhite@gentingcasinos.co.uk>
2021-01-11 09:45:53 +0000

    :bug: Hopefully fixing darkMode SSR by using a cookie

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { FC } from 'react';
 import { AppProps } from 'next/app';
 
-import "tailwindcss/tailwind.css";
+import 'tailwindcss/tailwind.css';
 import { ThemeProvider } from '~/context';
 
-const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider>
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
+const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ThemeProvider darkMode={pageProps.darkMode}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,32 @@
 import { FC } from 'react';
 
 import { AppShell, Container, IntroHero, BlogHero, Footer } from '~/components';
+import { getCookie } from '~/util';
 
-const IndexPage: FC = () => (
-  <AppShell>
-    <Container>
-      <IntroHero />
-    </Container>
-    <Container bg>
-      <BlogHero />
-    </Container>
-    <Container>
-      <Footer />
-    </Container>
-  </AppShell>
-);
+const IndexPage: FC = ({ darkMode }) => {
+  return (
+    <AppShell>
+      <Container>
+        <IntroHero />
+      </Container>
+      <Container bg>
+        <BlogHero />
+      </Container>
+      <Container>
+        <Footer />
+      </Container>
+    </AppShell>
+  );
+};
 
 export default IndexPage;
+
+export async function getServerSideProps({ req }) {
+  const darkMode = getCookie(req, 'darkMode');
+
+  return {
+    props: {
+      darkMode,
+    }, // will be passed to the page component as props
+  };
+}
