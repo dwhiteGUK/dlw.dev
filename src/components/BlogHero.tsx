bd0b584d79ecfa468deb44e7ee4e217118bd0a47
Darren White <dwhite@gentingcasinos.co.uk>
2021-01-31 16:31:13 +0000

    :green_heart: fix type issues breaking build

--- src/components/BlogHero.tsx
+++ src/components/BlogHero.tsx
@@ -1,7 +1,10 @@
 import { BlogItem } from '~/components';
 
 type Post = {
-  link: string;
+  slug: string;
+  title: string;
+  snippet: string;
+  timestamp: number;
 };
 
 const BlogHero = ({ posts }: { posts: []}) => {
@@ -10,7 +13,7 @@ const BlogHero = ({ posts }: { posts: []}) => {
       <h2 className="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl sm:leading-10">Blog</h2>
       <div className="mt-6 grid gap-16 lg:grid-cols-2 lg:gap-5">
         {posts.slice(0, 4).map((post: Post) => (
-          <BlogItem key={post.link} post={post} />
+          <BlogItem key={post.slug} post={post} />
         ))}
       </div>
     </>
