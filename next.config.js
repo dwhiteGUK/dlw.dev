493060fea93125f96421c2b26f89931388617052
Darren White <dwhite@gentingcasinos.co.uk>
2021-02-01 10:34:00 +0000

    :zap: replace react with preact in prod

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 module.exports = {
-  webpack: (config, { isServer }) => {
+  webpack: (config, { dev, isServer }) => {
     // extend your webpack configuration here
     config.resolve.alias['~'] = path.join(__dirname, './src');
 
@@ -9,6 +9,15 @@ module.exports = {
       config.node = {
         fs: 'empty',
       };
+    }
+
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
     }
 
     return config;
