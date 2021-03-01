3c603da40bff8465bb088089f638f7a4e03df00f
Darren White <dwhite@gentingcasinos.co.uk>
2021-03-01 10:18:32 +0000

    :speech_balloon: update intro on homepage

--- src/components/IntroHero.tsx
+++ src/components/IntroHero.tsx
@@ -16,14 +16,15 @@ const IntroHero: FC = () => {
         />
       </div>
       <div className="">
-        <div className="text-base leading-6 font-semibold uppercase tracking-wider text-gray-500">Introduction</div>
+        <div className="text-base leading-6 font-semibold uppercase tracking-wider text-gray-500">Welcome, I'm Darren White.</div>
         <Heading1 className="text-3xl leading-9 tracking-tight font-extrabold sm:text-4xl sm:leading-10">
-          html, css and js developer with over 15 years experience.
+          A web developer with 15+ years of experience. 
         </Heading1>
         <p className="mt-3 text-lg leading-7 text-gray-700 dark:text-gray-300">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a
-          scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat quisque ut interdum tincidunt
-          duis.
+          I build websites using HTML, CSS and JavaScript, Often with React/Next.js.  
+        </p>
+        <p className="mt-3 text-lg leading-7 text-gray-700 dark:text-gray-300">
+          It's not just the front end, I can spin up services in AWS or a solution with Nodejs.
         </p>
       </div>
     </div>
